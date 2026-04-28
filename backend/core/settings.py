eadb75e9896d7b1d8ae6c07a27b0a31f56480da7
Darshan S <darshans2727@gmail.com>
2026-04-28 13:33:19 +0530

    chore: update settings to use environment variables for SECRET_KEY and DEBUG

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--sbr75u3zjy3!_f*4m@43)rjm%34_7k27fl)e7r)g3$^3ywi1%'
+SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-dev-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ['playto-payout-engine-hmub.onrender.com',
                  'playto-payout.darshans.site', '127.0.0.1', 'localhost']
